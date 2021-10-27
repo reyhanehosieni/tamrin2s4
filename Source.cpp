09b778693d8729a0775ae7b41b092e9ee7789832
reyhanehosieni <92735882+reyhanehosieni@users.noreply.github.com>
2021-10-27 10:55:08 +0330

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	int i = 27;
+	int j = 22;
+	cout << "adad vared konid" << endl;
+	cin >> N;
+	system("pause");
+	for (int i = 0; i<N; i++)
+	{
+		cout << char(i) << char(j);
+	}
+	cout << endl;
+	system("pause");
+
+	return 0;
+}
